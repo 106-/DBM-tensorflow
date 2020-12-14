793d586df9bec4f411da2a2488b8730856687766
106 <Segmentation-fault@yandex.ua>
2020-12-15 08:44:21 +0900

    4層(難しめ)の学習エポックを2,000にする

--- script/4-layer_hard/2-smci/double.sh
+++ script/4-layer_hard/2-smci/double.sh
@@ -10,5 +10,5 @@ mkdir -p $DIR
 
 for i in `seq $1`
 do
-    ./train_main.py ./config/4-layer_hard/2-smci/double.json 1000 -d $DIR
+    ./train_main.py ./config/4-layer_hard/2-smci/double.json 2000 -d $DIR
 done
