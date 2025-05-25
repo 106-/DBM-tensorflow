9df4ce4161c71cd1c360a33c6951854387bb7d36
106- <segmentation-fault@yandex.com>
2025-05-25 20:47:14 +0900

    この変更は余計なので戻す

--- script/3-layer/1-smci/double.sh
+++ script/3-layer/1-smci/double.sh
@@ -6,16 +6,9 @@ if [ $# -ne 1 ]; then
 fi
 
 DIR="./results/"`date +%Y-%m-%d_%H-%M-%S`"_double_3-layer_1-smci/"
-echo "Results directory: $DIR"
 mkdir -p $DIR
-echo "Contents of results directory after creation:"
-ls -la $DIR
 
 for i in `seq $1`
 do
-    echo "Starting iteration $i"
-    ../../../train_main.py ../../../config/3-layer/1-smci/double.json 1 -d $DIR > "$DIR/train_main_iteration_$i.log" 2>&1
-    echo "Finished iteration $i"
-    echo "Contents of results directory after iteration $i:"
-    ls -la $DIR
+    ./train_main.py ./config/3-layer/1-smci/double.json 5000 -d $DIR
 done
